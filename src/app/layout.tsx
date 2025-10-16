2dcbb9f9a000ba17f4a12265fe98ab98675754ac
j1munoz <jimmymunoz397@gmail.com>
2025-10-16 11:04:21 -0700

    Added the striped wall

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,9 @@ type Props = {
 const RootLayout = async ({ children }: Props) => {
   return (
     <html lang="en" className="h-full">
-      <body className={`${poppins.variable} flex h-full flex-col lg:flex-row`}>
+      <body
+        className={`${poppins.variable} flex h-full flex-col bg-[#FFEFE2] lg:flex-row`}
+      >
         <div className="flex h-full w-full">{children}</div>
       </body>
     </html>
--- src/components/live/index.tsx
+++ src/components/live/index.tsx
@@ -8,12 +8,14 @@ import Committees from "./committees";
 import Judges from "./judges";
 import FAQ from "./faq";
 import Footer from "./footer";
+import StripeWall from "@/components/live/stripewall";
 
 const Live = () => {
   return (
     <>
       <Landing />
       <About />
+      <StripeWall />
       <Tracks />
       <Schedule />
       <Sponsors />
