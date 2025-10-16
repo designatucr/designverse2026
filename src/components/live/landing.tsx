528d5599b80d62b9ee3e3c8566104e3859578e91
shuuvi <janeleebmes@gmail.com>
2025-10-15 22:15:08 -0700

    added timer. bakery. sign, sky bg

--- src/components/live/landing.tsx
+++ src/components/live/landing.tsx
@@ -10,50 +10,54 @@ import grass2 from "@/public/landing/grass2.webp";
 
 const Landing = () => {
   return (
-    <div className="flex flex-col items-center w-screen bg-gradient-to-b from-landing-blue-100 to-landing-orange-100">
-      <div className="relative w-full flex justify-center z-20">
-        <Image src = {cloud1} alt = "cloud" className="absolute w-1/6 top-24 left-0"/>
-        <Image src = {cloud2} alt = "cloud" className="absolute w-[12%] top-56 right-0"/>
+    <div className="flex w-screen flex-col items-center bg-gradient-to-b from-landing-blue-100 to-landing-orange-100">
+      <div className="relative z-20 flex w-full justify-center">
+        <Image
+          src={cloud1}
+          alt="cloud"
+          className="absolute left-0 top-24 w-1/6"
+        />
+        <Image
+          src={cloud2}
+          alt="cloud"
+          className="absolute right-0 top-56 w-[12%]"
+        />
       </div>
-      <Image src = {logo} alt = "dv logo" className="w-1/5 pt-20 pb-5"/>
+      <Image src={logo} alt="dv logo" className="w-1/5 pb-5 pt-20" />
 
-      <div className="relative flex flex-col items-center justify-center w-1/2">
-        <p className="text-landing-brown-200 z-30 text-xl font-bold pt-2">
+      <div className="relative flex w-1/2 flex-col items-center justify-center">
+        <p className="z-30 pt-2 text-xl font-bold text-landing-brown-200">
           University of California, Riverside | May 9-10, 2026
-        </p>   
-        <Image src = {timer} alt = "timer" className="absolute z-0 w-4/5"/>
+        </p>
+        <Image src={timer} alt="timer" className="absolute z-0 w-4/5" />
         <div className="z-10">
           <Countdown
             classNames={{
               unit: "text-landing-brown-200",
               background: "bg-landing-brown-100",
               digit: "text-white",
-          }}
-        />
+            }}
+          />
         </div>
       </div>
-      
-      <div className="relative w-full flex justify-center z-20">
-        <Image
-          src={bakery}
-          alt="bakery"
-          className="w-[52%] z-20"
-        />
+
+      <div className="relative z-20 flex w-full justify-center">
+        <Image src={bakery} alt="bakery" className="z-20 w-[52%]" />
         <Image
           src={sign}
           alt="wooden board sign"
-          className="absolute w-[8%] right-[39%] -bottom-24 z-30"
+          className="absolute -bottom-24 right-[39%] z-30 w-[8%]"
         />
       </div>
       <Image
         src={grass2}
         alt="grass box"
-        className="absolute -bottom-[30%] left-48 w-1/5 z-30"
+        className="absolute -bottom-[30%] left-48 z-30 w-1/5"
       />
       <Image
         src={grass2}
         alt="grass box"
-        className="absolute -bottom-[30%] right-48 w-1/5 z-30"
+        className="absolute -bottom-[30%] right-48 z-30 w-1/5"
       />
     </div>
   );
