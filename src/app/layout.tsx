f263ecf3ad0d6c165d4ee4aca43de93990216f7e
j1munoz <jimmymunoz397@gmail.com>
2025-10-17 13:57:16 -0700

    Changed color values

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ const RootLayout = async ({ children }: Props) => {
   return (
     <html lang="en" className="h-full">
       <body
-        className={`${poppins.variable} flex h-full flex-col bg-[#FFEFE2] lg:flex-row`}
+        className={`${poppins.variable} flex h-full flex-col bg-landing-beige-100 lg:flex-row`}
       >
         <div className="flex h-full w-full">{children}</div>
       </body>
