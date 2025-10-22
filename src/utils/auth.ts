23ed341280cf349cf45407d1a3d645eb24bf1922
Sweden <sweden.agunenye1@gmail.com>
2025-10-22 00:38:39 -0700

    updated firebase private key string handling to attempt build fix w/ parsing

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -11,7 +11,8 @@ export const options: NextAuthOptions = {
     credential: cert({
       projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
       clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY,
+      privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY ? process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY.replace(/\n/gm, "\n")
+: undefined,
     }),
   }) as Adapter,
   providers: [
