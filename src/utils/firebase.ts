34726a5b737451451ac1cd42f6e81587038df9d6
Sweden <sweden.agunenye1@gmail.com>
2025-10-21 17:12:38 -0700

    updated keys

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,6 +1,17 @@
+import admin from "firebase-admin";
+// import serviceAccount from "./designverse2026-475822-efc1bbfaeda9.json";
+
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// admin.initializeApp({
+//   credential: admin.credential.cert({
+//     projectId: process.env.PROJECT_ID,
+//     clientEmail: process.env.CLIENT_EMAIL,
+//     privateKey: process.env.PRIVATE_KEY,
+//   }),
+//   databaseURL: process.env.FIREBASE_DB_URL,
+// });
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
