f751b22926d18635d0fbb988c65d963f9b854d51
Sweden <sweden.agunenye1@gmail.com>
2025-10-21 17:46:45 -0700

    had to rebase to drop commit with credentials embedded in json

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
