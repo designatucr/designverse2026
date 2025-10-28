773b9573dbf146a502b7efc547f8c5eff7f97289
shuuvi <janeleebmes@gmail.com>
2025-10-28 14:39:59 -0700

    changed private key function

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -5,13 +5,15 @@ import { cert } from "firebase-admin/app";
 import { NextAuthOptions, Session } from "next-auth";
 import type { Adapter } from "next-auth/adapters";
 
+const { privateKey } = JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY ? "" : "");
+
 export const options: NextAuthOptions = {
   // eslint-disable-next-line new-cap
   adapter: FirestoreAdapter({
     credential: cert({
       projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
       clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY,
+      privateKey,
     }),
   }) as Adapter,
   providers: [
