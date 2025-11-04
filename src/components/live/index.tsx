8eaa686da56a20ccb5e210237b270e1cfdafa96b
Sweden <sweden.agunenye1@gmail.com>
2025-11-03 16:25:42 -0800

    hid rest of landing, need to add info

--- src/components/live/index.tsx
+++ src/components/live/index.tsx
@@ -1,12 +1,12 @@
 import Landing from "./landing";
 import About from "./about";
-import Schedule from "./schedule";
-import Tracks from "./tracks";
-import Sponsors from "./sponsors";
-import Team from "./team";
-import Committees from "./committees";
-import Judges from "./judges";
-import FAQ from "./faq";
+// import Schedule from "./schedule";
+// import Tracks from "./tracks";
+// import Sponsors from "./sponsors";
+// import Team from "./team";
+// import Committees from "./committees";
+// import Judges from "./judges";
+// import FAQ from "./faq";
 import Footer from "./footer";
 import StripeWall from "@/components/live/stripewall";
 
@@ -16,13 +16,13 @@ const Live = () => {
       <Landing />
       <About />
       <StripeWall />
-      <Tracks />
+      {/* <Tracks />
       <Schedule />
       <Sponsors />
       <Team />
       <Committees />
       <Judges />
-      <FAQ />
+      <FAQ /> */}
       <Footer />
     </>
   );
