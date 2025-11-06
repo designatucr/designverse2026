769a4997e50437b5b3177a159a55660a4f94f7ee
j1munoz <jimmymunoz397@gmail.com>
2025-11-05 23:09:22 -0800

    Coming soon section

--- src/components/live/landing.tsx
+++ src/components/live/landing.tsx
@@ -1,17 +1,18 @@
-import Countdown from "../ui/countdown";
+// import Countdown from "../ui/countdown";
 import Image from "next/image";
 import cloud1 from "@/public/landing/cloud1.webp";
 import cloud2 from "@/public/landing/cloud2.webp";
 import logo from "@/public/landing/dv_logo.webp";
 import timer from "@/public/landing/timer.webp";
-import sign from "@/public/landing/sign.webp";
-import bakery from "@/public/landing/bakery.webp";
+// import sign from "@/public/landing/sign.webp";
+// import bakery from "@/public/landing/bakery.webp";
 import grass from "@/public/landing/grass.webp";
+import bakery2 from "@/public/landing/bakery2.svg";
 import Link from "next/link";
 
 const Landing = () => {
   return (
-    <div className="flex flex-col items-center bg-gradient-to-b from-landing-blue-100 to-landing-orange-100">
+    <div className="flex w-full flex-col items-center bg-gradient-to-b from-landing-blue-100 to-landing-orange-100">
       <Link
         href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2026-season&utm_content=white"
         target="_blank"
@@ -37,42 +38,54 @@ const Landing = () => {
           className="absolute right-0 top-56 w-[12%]"
         />
       </div>
-      <Image src={logo} alt="dv logo sign" className="w-1/4 pt-20" />
-
-      <div className="relative flex w-1/2 flex-col items-center justify-center">
-        <p className="z-30 pt-2 text-xl font-bold text-landing-brown-200">
-          University of California, Riverside | May 9-10, 2026
-        </p>
-        <Image src={timer} alt="timer bg" className="absolute z-0 w-4/5" />
-        <div className="z-10">
-          <Countdown
+      <div className="sm:mt-34 relative z-20 mt-20 flex w-[90%] flex-col items-center justify-center md:mt-36 lg:mt-52">
+        <div className="relative flex w-full flex-col items-center">
+          <Image
+            src={logo}
+            alt="dv logo sign"
+            className="w-1/4 -translate-y-[80%]"
+          />
+          <Image
+            src={timer}
+            alt="timer bg"
+            className="absolute bottom-[70%] z-0 w-[60%]"
+          />
+          {/* <Countdown
             classNames={{
               unit: "text-landing-brown-200",
               background: "bg-landing-brown-100",
               digit: "text-white",
             }}
-          />
+          /> */}
+          <div className="absolute top-[3%] z-30 flex flex-col items-center gap-1 pt-2 text-landing-brown-200 sm:top-[5%] sm:gap-2 md:top-[7%]">
+            <p className="text-wrap text-[0.45rem] font-bold sm:text-xs md:text-sm lg:text-xl xl:text-3xl">
+              University of California, Riverside | May 9-10, 2026
+            </p>
+            <p className="text-[0.45rem] sm:text-xs md:text-sm lg:text-2xl">
+              A design-a-thon near you!
+            </p>
+          </div>
+          <Image src={bakery2} alt="bakery shop" className="z-20 w-[85%]" />
+        </div>
+        <Image
+          src={grass}
+          alt="left grass box"
+          className="absolute bottom-0 left-0 z-30 w-[25%] -translate-x-[24%]"
+        />
+        <Image
+          src={grass}
+          alt="right grass box"
+          className="absolute bottom-0 right-0 z-30 w-[26%] translate-x-[21%] -scale-x-100"
+        />
+        <div className="absolute top-[60%] z-30 flex w-[90%] flex-col items-center justify-center gap-2 text-landing-brown-300 md:gap-5">
+          <div className="text-lg font-bold sm:text-3xl md:text-5xl lg:text-7xl">
+            COMING SOON.
+          </div>
+          <div className="text-sm md:text-xl lg:text-3xl">
+            Something new is brewing...
+          </div>
         </div>
       </div>
-
-      <div className="relative z-20 flex w-full justify-center">
-        <Image src={bakery} alt="bakery shop" className="z-20 w-[52%]" />
-        <Image
-          src={sign}
-          alt="wooden board sign"
-          className="absolute -bottom-20 right-[39%] z-30 w-[8%]"
-        />
-      </div>
-      <Image
-        src={grass}
-        alt="left grass box"
-        className="absolute -bottom-[29%] left-48 z-30 w-1/5"
-      />
-      <Image
-        src={grass}
-        alt="right grass box"
-        className="absolute -bottom-[29%] right-48 z-30 w-1/5"
-      />
     </div>
   );
 };
