b62ce7908dc7f35bfb451bc7117ef9defd91c17b
adidi-c <mradidi18@gmail.com>
2025-11-10 05:26:46 -0800

    ran prettier

--- src/components/live/about.tsx
+++ src/components/live/about.tsx
@@ -13,15 +13,15 @@ const About = () => {
         id="about"
         className="z-20 w-full items-start justify-center bg-[#fbe8d5] py-32 md:py-96"
       >
-        <div className="pl-[8vw] pr-[8vw] font-semibold w-3/4 ">
+        <div className="w-3/4 pl-[8vw] pr-[8vw] font-semibold">
           <p className="mb-4 pb-12 text-3xl text-[#845d3e] lg:text-5xl">
             ABOUT US
           </p>
-          <div className="relative bg-white p-6 sm:p-12 py-24 sm:mb-72 md:mb-36 lg:mb-24 xl:mb-24 shadow-[5px_5px_5px_rgba(0,0,0,0.3)] text-landing-brown-400 ">
-            <div className="absolute top-0 left-0 h-8 w-28 bg-blue-200/60 rotate-[-38deg] translate-x-[-25%] translate-y-[15%]" />
-            <div className="absolute top-0 right-0 h-8 w-28 bg-blue-200/60 rotate-[38deg] translate-x-[25%] translate-y-[-20%]" />
-            <div className="absolute bottom-0 left-0 h-8 w-28 bg-blue-200/60 rotate-[38deg] translate-x-[-30%] translate-y-[-20%]" />
-            <div className="absolute bottom-0 right-0 h-8 w-28 bg-blue-200/60 rotate-[-38deg] translate-x-[40%] translate-y-[20%]" />
+          <div className="relative bg-white p-6 py-24 text-landing-brown-400 shadow-[5px_5px_5px_rgba(0,0,0,0.3)] sm:mb-72 sm:p-12 md:mb-36 lg:mb-24 xl:mb-24">
+            <div className="absolute left-0 top-0 h-8 w-28 translate-x-[-25%] translate-y-[15%] rotate-[-38deg] bg-blue-200/60" />
+            <div className="absolute right-0 top-0 h-8 w-28 translate-x-[25%] translate-y-[-20%] rotate-[38deg] bg-blue-200/60" />
+            <div className="absolute bottom-0 left-0 h-8 w-28 translate-x-[-30%] translate-y-[-20%] rotate-[38deg] bg-blue-200/60" />
+            <div className="absolute bottom-0 right-0 h-8 w-28 translate-x-[40%] translate-y-[20%] rotate-[-38deg] bg-blue-200/60" />
             <p className="text-sm font-medium lg:text-lg">
               DesignVerse is the first and only <b>24-hour beginner-friendly</b>{" "}
               Design-a-Thon hosted by the University of California, Riverside.
@@ -44,27 +44,23 @@ const About = () => {
         </div>
       </section>
       <div className="relative">
-      <Image
-        src={bunny}
-        alt="bunny"
-        className="absolute bottom-0.5 left-[75%] bottom-[10%] left-[60%] w-2/5 pb-48 sm:bottom-[10%] sm:left-[60%] sm:w-2/5 md:bottom-[5%] md:left-[60%] md:w-1/3 lg:left-[55%] lg:w-auto xl:bottom-[10%] xl:right-[12%] xl:w-auto"
-        // className="absolute bottom-0 left-[75%] w-1/4 pb-48 sm:bottom-[10%] sm:left-[70%] sm:w-[20%] md:bottom-[5%] md:left-[65%] md:w-[15%] xl:bottom-[10%] xl:left-[60%] xl:w-[10%]"
-        // className="absolute bottom-0 left-[75%] w-1/4 sm:w-[20%] md:w-[15%] xl:w-[10%] pb-48 sm:pb-32 md:pb-16 xl:pb-8"
-        // className="absolute bottom-0.5 left-[75%] w-1/5 pb-48 sm:bottom-[10%] sm:left-[60%] sm:w-2/5 md:bottom-[5%] md:left-[60%] md:w-auto lg:left-[55%] lg:w-auto xl:bottom-[10%] xl:right-[12%] xl:w-auto sm:mt-16"
-
-      />
-      <Image
-        src={matcha}
-        alt="matcha"
-        className="z-40 absolute bottom-0.5 left-[70%] w-1/5 pb-48 sm:bottom-[10%] sm:right-[15%] sm:w-auto md:bottom-[5%] md:left-[60%] md:w-auto xl:bottom-[10%] xl:right-[12%] xl:w-auto"
-      />
-      <Image
-        src={taiyaki}
-        alt="taiyaki"
-        className="z-40 absolute bottom-0.5 left-[60%] w-1/5 pb-32 sm:bottom-[10%] sm:right-[15%] sm:w-auto md:bottom-[5%] md:left-[50%] md:w-auto xl:bottom-[10%] xl:right-[12%] xl:w-auto"
-      />
-      <div className="relative z-30 h-32 w-full bg-[#C3A47E]" />
-      <div className="relative z-30 h-48 w-full bg-[#A28561]" />
+        <Image
+          src={bunny}
+          alt="bunny"
+          className="absolute bottom-0.5 bottom-[10%] left-[60%] left-[75%] w-2/5 pb-48 sm:bottom-[10%] sm:left-[60%] sm:w-2/5 md:bottom-[5%] md:left-[60%] md:w-1/3 lg:left-[55%] lg:w-auto xl:bottom-[10%] xl:right-[12%] xl:w-auto"
+        />
+        <Image
+          src={matcha}
+          alt="matcha"
+          className="absolute bottom-0.5 left-[70%] z-40 w-1/5 pb-48 sm:bottom-[10%] sm:right-[15%] sm:w-auto md:bottom-[5%] md:left-[60%] md:w-auto xl:bottom-[10%] xl:right-[12%] xl:w-auto"
+        />
+        <Image
+          src={taiyaki}
+          alt="taiyaki"
+          className="absolute bottom-0.5 left-[60%] z-40 w-1/5 pb-32 sm:bottom-[10%] sm:right-[15%] sm:w-auto md:bottom-[5%] md:left-[50%] md:w-auto xl:bottom-[10%] xl:right-[12%] xl:w-auto"
+        />
+        <div className="relative z-30 h-32 w-full bg-[#C3A47E]" />
+        <div className="relative z-30 h-48 w-full bg-[#A28561]" />
       </div>
 
       {/* tried using below code to implement the (alleged) gradient from figma, but for whatever reason it disappears whenever i scroll, 
