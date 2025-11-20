fe76e7bb80afa28e52b2f816896c2dc7d4ebd506
adidi-c <mradidi18@gmail.com>
2025-11-20 09:43:15 -0800

    responsified about us(bunny, matcha, taiyaki, poster)

--- src/components/live/about.tsx
+++ src/components/live/about.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import bunny from "@/public/landing/bunny.svg";
 import matcha from "@/public/landing/matcha.svg";
 import taiyaki from "@/public/landing/taiyaki.svg";
+// eslint-disable-next-line new-cap
 const inter = Inter({ subsets: ["latin"] });
 
 const About = () => {
@@ -11,58 +12,60 @@ const About = () => {
     <div className={`relative ${inter.className}`}>
       <section
         id="about"
-        className="z-20 w-full items-start justify-center bg-[#fbe8d5] py-32 md:py-96"
+        className="z-20 w-full items-start justify-center bg-[#fbe8d5] py-32 md:py-64"
       >
-        <div className="w-3/4 pl-[8vw] pr-[8vw] font-semibold">
-          <p className="mb-4 pb-12 text-3xl text-[#845d3e] lg:text-5xl">
-            ABOUT US
-          </p>
-          <div className="relative bg-white p-6 py-24 text-landing-brown-400 shadow-[5px_5px_5px_rgba(0,0,0,0.3)] sm:mb-72 sm:p-12 md:mb-36 lg:mb-24 xl:mb-24">
-            <div className="absolute left-0 top-0 h-8 w-28 translate-x-[-25%] translate-y-[15%] rotate-[-38deg] bg-blue-200/60" />
-            <div className="absolute right-0 top-0 h-8 w-28 translate-x-[25%] translate-y-[-20%] rotate-[38deg] bg-blue-200/60" />
-            <div className="absolute bottom-0 left-0 h-8 w-28 translate-x-[-30%] translate-y-[-20%] rotate-[38deg] bg-blue-200/60" />
-            <div className="absolute bottom-0 right-0 h-8 w-28 translate-x-[40%] translate-y-[20%] rotate-[-38deg] bg-blue-200/60" />
-            <p className="text-sm font-medium lg:text-lg">
-              DesignVerse is the first and only <b>24-hour beginner-friendly</b>{" "}
-              Design-a-Thon hosted by the University of California, Riverside.
-              Designers are challenged to create the interface of an app or
-              website through a given prompt, which would then be demoed and
-              presented to a panel of industry professionals.
-              <br />
-              <br />
-              At DesignVerse, our goal is to empower our community of students
-              who are interested in design-like thinking and provide career
-              developmental opportunities through our <b>workshops</b>,{" "}
-              <b>mentorship</b>, and <b>networking events</b>.
-              <br />
-              <br />
-              We welcome all undergraduate, graduate and high school students
-              for a lively weekend filled with creativity, networking,
-              mentorship, and more!
-            </p>
+        <div className="md:w-7/8 pl-[8vw] pr-[8vw] font-semibold sm:w-full">
+          <Title>ABOUT US</Title>
+          <div className="flex flex-col items-start gap-8 md:flex-row md:gap-12">
+            <div className="relative flex-1">
+              <div className="relative bg-white p-6 py-24 text-landing-brown-400 shadow-[5px_5px_5px_rgba(0,0,0,0.3)] sm:p-12">
+                <div className="absolute left-0 top-0 h-8 w-28 translate-x-[-25%] translate-y-[15%] rotate-[-38deg] bg-blue-200/60" />
+                <div className="absolute right-0 top-0 h-8 w-28 translate-x-[25%] translate-y-[-20%] rotate-[38deg] bg-blue-200/60" />
+                <div className="absolute bottom-0 left-0 h-8 w-28 translate-x-[-30%] translate-y-[-20%] rotate-[38deg] bg-blue-200/60" />
+                <div className="absolute bottom-0 right-0 h-8 w-28 translate-x-[30%] translate-y-[20%] rotate-[-38deg] bg-blue-200/60" />
+                <p className="text-sm font-medium lg:text-lg">
+                  DesignVerse is the first and only{" "}
+                  <b>24-hour beginner-friendly</b> Design-a-Thon hosted by the
+                  University of California, Riverside. Designers are challenged
+                  to create the interface of an app or website through a given
+                  prompt, which would then be demoed and presented to a panel of
+                  industry professionals.
+                  <br />
+                  <br />
+                  At DesignVerse, our goal is to empower our community of
+                  students who are interested in design-like thinking and
+                  provide career developmental opportunities through our{" "}
+                  <b>workshops</b>, <b>mentorship</b>, and{" "}
+                  <b>networking events</b>.
+                  <br />
+                  <br />
+                  We welcome all undergraduate, graduate and high school
+                  students for a lively weekend filled with creativity,
+                  networking, mentorship, and more!
+                </p>
+              </div>
+            </div>
+            <div className="relative flex w-1/2 max-w-[450px] items-center justify-center place-self-end sm:w-1/2 sm:place-self-end md:w-1/3 md:pt-40 lg:w-1/3 lg:pt-40 xl:w-1/3">
+              <Image src={bunny} alt="bunny" className="h-auto w-full" />
+            </div>
           </div>
         </div>
-      </section>
-      <div className="relative">
-        <Image
-          src={bunny}
-          alt="bunny"
-          className="absolute bottom-0.5 bottom-[10%] left-[60%] left-[75%] w-2/5 pb-48 sm:bottom-[10%] sm:left-[60%] sm:w-2/5 md:bottom-[5%] md:left-[60%] md:w-1/3 lg:left-[55%] lg:w-auto xl:bottom-[10%] xl:right-[12%] xl:w-auto"
-        />
         <Image
           src={matcha}
           alt="matcha"
-          className="absolute bottom-0.5 left-[70%] z-40 w-1/5 pb-48 sm:bottom-[10%] sm:right-[15%] sm:w-auto md:bottom-[5%] md:left-[60%] md:w-auto xl:bottom-[10%] xl:right-[12%] xl:w-auto"
+          className="absolute bottom-8 right-[33%] z-40 max-w-[120px] pb-52 sm:right-[30%] sm:w-1/6 md:right-[25%] md:max-w-[120px] lg:right-[20%] lg:max-w-[140px] xl:right-[25%] xl:max-w-[150px]"
         />
         <Image
           src={taiyaki}
           alt="taiyaki"
-          className="absolute bottom-0.5 left-[60%] z-40 w-1/5 pb-32 sm:bottom-[10%] sm:right-[15%] sm:w-auto md:bottom-[5%] md:left-[50%] md:w-auto xl:bottom-[10%] xl:right-[12%] xl:w-auto"
+          className="absolute bottom-20 right-[60%] z-40 max-w-[115px] pb-32 sm:right-[45%] sm:w-1/6 md:max-w-[130px] lg:max-w-[140px]"
         />
-        <div className="relative z-30 h-32 w-full bg-[#C3A47E]" />
+      </section>
+      <div className="relative">
+        <div className="bottom relative right-[40%] w-1/5 place-self-end"></div>
+        <div className="2xl:-mt- relative z-30 -mt-48 h-32 w-full bg-[#C3A47E] md:-mt-72 lg:-mt-72 xl:-mt-80" />
         <div className="relative z-30 h-48 w-full bg-[#A28561]" />
       </div>
-
       {/* tried using below code to implement the (alleged) gradient from figma, but for whatever reason it disappears whenever i scroll, 
       might be because my monitor is ridiculously large ? idk i made it a solid color instead */}
       {/* <div className="w-full h-48 bg-gradient-to-b from-[#B99C78] to-[#A28561]" /> */}
