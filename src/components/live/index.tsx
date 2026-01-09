5c165748c4b3924debe54b894727c07d6dcd99d7
Charleen Chen <charleenc485@gmail.com>
2026-01-09 13:19:19 -0800

    fix

--- src/components/live/index.tsx
+++ src/components/live/index.tsx
@@ -2,7 +2,7 @@ import Landing from "./landing";
 // import About from "./about";
 // import Schedule from "./schedule";
 // import Tracks from "./tracks";
-import Resources from "./resources";
+// import Resources from "./resources";
 // import Sponsors from "./sponsors";
 // import Team from "./team";
 // import Committees from "./committees";
