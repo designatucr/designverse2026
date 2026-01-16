7169a731e08ac5c67690e03a8fcfac749c5aebbc
Rachel Chan <rchan2023@gmail.com>
2026-01-16 14:49:53 -0800

    new schedule design

--- src/components/live/index.tsx
+++ src/components/live/index.tsx
@@ -2,11 +2,12 @@ import Landing from "./landing";
 import About from "./about";
 // import Schedule from "./schedule";
 // import Tracks from "./tracks";
-// import Sponsors from "./sponsors";
+import Resources from "./resources";
+import Sponsors from "./sponsors";
 // import Team from "./team";
 // import Committees from "./committees";
 // import Judges from "./judges";
-// import FAQ from "./faq";
+import FAQ from "./faq";
 import Footer from "./footer";
 // import StripeWall from "@/components/live/stripewall";
 
@@ -15,15 +16,16 @@ const Live = () => {
     <>
       <Landing />
       <About />
+      <Sponsors />
+      <Resources />
       {/* 
       <StripeWall /> */}
-      {/* <Tracks />
+      {/* <Tracks />}
       <Schedule />
-      <Sponsors />
       <Team />
       <Committees />
-      <Judges />
-      <FAQ /> */}
+      <Judges /> */}
+      <FAQ />
       <Footer />
     </>
   );
--- src/components/live/schedule/events.tsx
+++ src/components/live/schedule/events.tsx
@@ -18,14 +18,14 @@ const Events = ({ events, totalDays }: props) => {
   );
 
   return (
-    <div className="flex flex-col items-center justify-center">
-      <div className="mx-auto grid w-11/12 grid-cols-7 items-center justify-between gap-2">
+    <div className="mx-auto flex w-5/6 flex-col items-center justify-center rounded-xl border-8 border-landing-brown-700 bg-landing-grey-100 p-6 lg:border-[16px] lg:p-10">
+      <div className="mx-auto flex w-full items-center justify-between gap-2 overflow-x-scroll lg:w-11/12">
         {totalDays.map((day) => (
           <button
             key={day}
-            className={`flex justify-center p-2 text-xs focus:outline-none sm:text-sm lg:mb-8 lg:mt-2 lg:text-lg ${
+            className={`flex w-40 min-w-20 justify-center rounded-md p-2 text-xs font-bold focus:outline-none sm:text-sm lg:mb-8 lg:mt-2 lg:rounded-lg lg:text-lg ${
               selectedDay === day
-                ? "bg-landing-brown-300 text-white"
+                ? "bg-white text-landing-brown-500"
                 : "bg-landing-brown-600 text-landing-brown-500"
             }`}
             onClick={() => setSelectedDay(day)}
@@ -58,7 +58,7 @@ const Events = ({ events, totalDays }: props) => {
               .map(({ start, summary, description, location }, index) => (
                 <div
                   key={index}
-                  className="font-workSans mb-2 grid grid-cols-3 items-center justify-between rounded-md bg-landing-brown-300 px-4 py-3 text-sm font-semibold text-white sm:mb-2 lg:mb-4 lg:rounded-xl lg:p-8 lg:text-lg"
+                  className="font-workSans mb-2 grid items-center justify-between rounded-md px-4 py-3 text-sm font-semibold text-white sm:mb-2 lg:mb-4 lg:grid-cols-3 lg:rounded-xl lg:p-8 lg:text-lg"
                 >
                   <p className="flex">{summary}</p>
                   <p className="flex">{description.split("\n")[0].slice(1)}</p>
