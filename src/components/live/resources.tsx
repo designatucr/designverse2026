65bed600ae52abacf363417d58b59b6dce2421ac
j1munoz <jimmymunoz397@gmail.com>
2026-02-27 09:12:32 -0800

    Final changes

--- src/components/live/resources.tsx
+++ src/components/live/resources.tsx
@@ -57,42 +57,63 @@ const Resources = () => {
       <p className="flex w-3/4 justify-center rounded-t-2xl bg-landing-brown-300 p-12 font-inter text-2xl font-bold text-white md:text-7xl">
         RESOURCES
       </p>
-      <div className="relative grid w-3/4 grid-cols-1 gap-0 border-2 border-x-white lg:grid-cols-3">
-        {resources.map(({ link, title, description, image }, index) => (
-          <div
-            className="relative flex flex-col items-center justify-center gap-8 bg-landing-glass-100/15 pt-10"
-            key={index}
-          >
-            <div className="absolute bottom-[43%] -z-10 block w-[60vw] px-5 [perspective:800px] sm:bottom-[36%] md:bottom-[39%] lg:hidden">
-              <div className="origin-top [transform-style:preserve-3d] [transform:rotateX(45deg)]">
-                <div className="h-20 bg-landing-brown-400" />
+      <div className="relative flex w-3/4 flex-col items-center justify-center">
+        <div className="relative grid w-full grid-cols-1 gap-0 border-x-2 border-white lg:grid-cols-3">
+          {resources.map(({ link, title, description, image }, index) => (
+            <div
+              className="relative flex flex-col items-center justify-center"
+              key={index}
+            >
+              <div className="relative flex h-[25vh] w-full flex-col items-center justify-center bg-landing-glass-100/15 md:h-[40vh] lg:h-[35vh]">
+                <Link
+                  href={link}
+                  target="_blank"
+                  className="md:h-2/12 flex w-[25vw] flex-col items-center hover:scale-105 hover:cursor-pointer sm:w-1/4 md:w-1/2 lg:w-1/2 xl:w-1/2"
+                >
+                  <Image src={image} className="rounded-full" alt="Resource" />
+                </Link>
+                <div className="absolute bottom-0 -z-10 block w-[50vw] translate-y-[71%] px-5 [perspective:800px] sm:w-[50vw] sm:translate-y-[73%] md:w-[44vw] md:translate-y-[76%] lg:hidden">
+                  <div className="origin-top [transform-style:preserve-3d] [transform:rotateX(70deg)] sm:[transform:rotateX(70deg)] md:[transform:rotateX(70deg)]">
+                    <div className="h-[20vh] bg-landing-brown-400 sm:h-[20vh] md:h-[37vh]" />
+                  </div>
+                </div>
+              </div>
+              <div className="h-2/3 w-full bg-landing-brown-300 p-8 text-center text-white lg:p-10">
+                <div className="mb-6 text-xl font-bold md:text-3xl">
+                  {title}
+                </div>
+                <div className="text-center lg:text-lg">{description}</div>
               </div>
             </div>
-            <Link
-              href={link}
-              target="_blank"
-              className="md:h-2/12 flex w-2/5 flex-col items-center hover:scale-105 hover:cursor-pointer md:w-2/12 lg:w-1/2"
-            >
-              <Image src={image} className="rounded-full" alt="Resource" />
-            </Link>
-            <div className="h-2/3 w-full bg-landing-brown-300 p-8 text-center text-white lg:p-12">
-              <div className="mb-6 text-xl font-bold md:text-3xl">{title}</div>
-              <div className="text-center lg:text-lg">{description}</div>
+          ))}
+          <div className="w-full bg-landing-brown-300 md:hidden lg:bg-gray-100" />
+          <div className="relative flex h-[25vh] items-center justify-center bg-landing-glass-100/15 lg:hidden">
+            <Image src={Turtle} alt="Turtle Bread" className="scale-[0.8]" />
+            <div className="absolute bottom-0 -z-10 block w-[45vw] translate-y-[75%] px-5 [perspective:800px] sm:w-[47vw] sm:translate-y-[73%] md:w-[44vw] md:translate-y-[76%] lg:hidden">
+              <div className="origin-top [transform-style:preserve-3d] [transform:rotateX(70deg)] sm:[transform:rotateX(70deg)] md:[transform:rotateX(70deg)]">
+                <div className="h-[30vh] bg-landing-brown-400 sm:h-[28vh] md:h-[37vh]" />
+              </div>
             </div>
           </div>
-        ))}
-        <div className="w-full bg-landing-brown-300 p-16 md:hidden lg:bg-gray-100" />
-        <div className="flex items-center justify-center lg:hidden">
-          <Image src={Turtle} alt="Turtle Bread" className="scale-[0.7]" />
+          {bakeryItems.map(({ image, alternative, sizing }, index) => (
+            <div
+              className="hidden flex-col items-center justify-center gap-8 bg-landing-glass-100/15 p-3 lg:flex lg:pt-10"
+              key={index}
+            >
+              <Image src={image} alt={alternative} className={sizing} />
+            </div>
+          ))}
         </div>
-        {bakeryItems.map(({ image, alternative, sizing }, index) => (
-          <div
-            className="hidden flex-col items-center justify-center gap-8 bg-landing-glass-100/15 p-3 lg:flex lg:pt-10"
-            key={index}
-          >
-            <Image src={image} alt={alternative} className={sizing} />
+        <div className="absolute -z-10 hidden [perspective:800px] md:block lg:top-[20%] lg:w-[55vw] xl:top-[19%] xl:w-[58vw]">
+          <div className="origin-top [transform-style:preserve-3d] [transform:rotateX(50deg)] xl:[transform:rotateX(48deg)]">
+            <div className="bg-landing-brown-400 lg:h-[18vh] xl:h-[18vh]" />
           </div>
-        ))}
+        </div>
+        <div className="absolute bottom-0 -z-10 hidden [perspective:800px] md:block lg:w-[56vw] lg:translate-y-[40%] xl:w-[58vw] xl:translate-y-[45%]">
+          <div className="origin-top [transform-style:preserve-3d] [transform:rotateX(50deg)] xl:[transform:rotateX(48deg)]">
+            <div className="bg-landing-brown-400 lg:h-[18vh] xl:h-[18vh]" />
+          </div>
+        </div>
       </div>
       <div className="w-3/4 bg-landing-brown-300 p-16 text-landing-brown-400 lg:bg-landing-brown-300 lg:text-white" />
     </div>
