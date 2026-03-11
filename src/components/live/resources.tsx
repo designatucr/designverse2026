2252ebee6f480beb8ca16b4fbdf06d7b32466126
j1munoz <jimmymunoz397@gmail.com>
2026-03-11 11:46:27 -0700

    Fixes

--- src/components/live/resources.tsx
+++ src/components/live/resources.tsx
@@ -54,29 +54,29 @@ const bakeryItems = [
 const Resources = () => {
   return (
     <div className="flex flex-col items-center justify-center p-16">
-      <p className="flex w-3/4 justify-center rounded-t-2xl bg-landing-brown-300 p-12 font-inter text-2xl font-bold text-white md:text-7xl">
+      <p className="flex w-4/5 justify-center rounded-t-2xl bg-landing-brown-300 p-12 font-inter text-2xl font-bold text-white md:text-7xl">
         RESOURCES
       </p>
-      <div className="relative flex w-3/4 flex-col items-center justify-center">
+      <div className="relative flex w-4/5 flex-col items-center justify-center">
         <div className="relative grid w-full grid-cols-1 gap-0 border-x-2 border-white lg:grid-cols-3">
           {resources.map(({ link, title, description, image }, index) => (
             <div
               className="relative flex flex-col items-center justify-center"
               key={index}
             >
-              <div className="relative flex h-[25vh] w-full flex-col items-center justify-center bg-landing-glass-100/15 md:h-[40vh] lg:h-[35vh]">
+              <div className="relative flex h-[20vh] w-full flex-col items-center justify-center bg-landing-glass-100/15 sm:h-[25vh] md:h-[40vh] lg:h-[40vh]">
                 <Link
                   href={link}
                   target="_blank"
-                  className="md:h-2/12 flex w-[25vw] flex-col items-center hover:scale-105 hover:cursor-pointer sm:w-1/4 md:w-1/2 lg:w-1/2 xl:w-1/2"
+                  className="flex w-full flex-col items-center justify-center hover:scale-105 hover:cursor-pointer"
                 >
-                  <Image src={image} className="rounded-full" alt="Resource" />
+                  <Image
+                    src={image}
+                    className="md:w-42 w-24 rounded-full sm:w-32 lg:w-32"
+                    alt="Resource"
+                  />
                 </Link>
-                <div className="absolute bottom-0 -z-10 block w-[50vw] translate-y-[71%] px-5 [perspective:800px] sm:w-[50vw] sm:translate-y-[73%] md:w-[44vw] md:translate-y-[76%] lg:hidden">
-                  <div className="origin-top [transform-style:preserve-3d] [transform:rotateX(70deg)] sm:[transform:rotateX(70deg)] md:[transform:rotateX(70deg)]">
-                    <div className="h-[20vh] bg-landing-brown-400 sm:h-[20vh] md:h-[37vh]" />
-                  </div>
-                </div>
+                <div className="sm:h-21 absolute bottom-0 -z-10 h-[32%] w-full translate-y-[1%] bg-landing-brown-400 [clip-path:polygon(12%_0%,87%_0%,_100%_100%,_0%_100%)] md:h-[32%] lg:hidden" />
               </div>
               <div className="h-2/3 w-full bg-landing-brown-300 p-8 text-center text-white lg:p-10">
                 <div className="mb-6 text-xl font-bold md:text-3xl">
@@ -87,13 +87,13 @@ const Resources = () => {
             </div>
           ))}
           <div className="w-full bg-landing-brown-300 md:hidden lg:bg-gray-100" />
-          <div className="relative flex h-[25vh] items-center justify-center bg-landing-glass-100/15 lg:hidden">
-            <Image src={Turtle} alt="Turtle Bread" className="scale-[0.8]" />
-            <div className="absolute bottom-0 -z-10 block w-[45vw] translate-y-[75%] px-5 [perspective:800px] sm:w-[47vw] sm:translate-y-[73%] md:w-[44vw] md:translate-y-[76%] lg:hidden">
-              <div className="origin-top [transform-style:preserve-3d] [transform:rotateX(70deg)] sm:[transform:rotateX(70deg)] md:[transform:rotateX(70deg)]">
-                <div className="h-[30vh] bg-landing-brown-400 sm:h-[28vh] md:h-[37vh]" />
-              </div>
-            </div>
+          <div className="relative flex h-[20vh] w-full flex-col items-center justify-center bg-landing-glass-100/15 sm:h-[25vh] md:h-[40vh] lg:hidden lg:h-[40vh]">
+            <Image
+              src={Turtle}
+              alt="Turtle Bread"
+              className="w-24 translate-y-[20%] sm:w-32 md:w-48 lg:w-32"
+            />
+            <div className="sm:h-21 absolute bottom-0 -z-10 h-[32%] w-full translate-y-[1%] bg-landing-brown-400 [clip-path:polygon(12%_0%,87%_0%,_100%_100%,_0%_100%)] md:h-[32%] lg:hidden" />
           </div>
           {bakeryItems.map(({ image, alternative, sizing }, index) => (
             <div
@@ -104,18 +104,10 @@ const Resources = () => {
             </div>
           ))}
         </div>
-        <div className="absolute -z-10 hidden [perspective:800px] md:block lg:top-[20%] lg:w-[55vw] xl:top-[19%] xl:w-[58vw]">
-          <div className="origin-top [transform-style:preserve-3d] [transform:rotateX(50deg)] xl:[transform:rotateX(48deg)]">
-            <div className="bg-landing-brown-400 lg:h-[18vh] xl:h-[18vh]" />
-          </div>
-        </div>
-        <div className="absolute bottom-0 -z-10 hidden [perspective:800px] md:block lg:w-[56vw] lg:translate-y-[40%] xl:w-[58vw] xl:translate-y-[45%]">
-          <div className="origin-top [transform-style:preserve-3d] [transform:rotateX(50deg)] xl:[transform:rotateX(48deg)]">
-            <div className="bg-landing-brown-400 lg:h-[18vh] xl:h-[18vh]" />
-          </div>
-        </div>
+        <div className="absolute top-[21%] -z-10 hidden h-[32%] w-full translate-y-[1%] bg-landing-brown-400 [clip-path:polygon(6%_0%,94%_0%,_100%_100%,_0%_100%)] sm:h-64 md:h-28 lg:block" />
+        <div className="absolute bottom-0 -z-10 hidden h-[32%] w-full translate-y-[3%] bg-landing-brown-400 [clip-path:polygon(6%_0%,94%_0%,_100%_100%,_0%_100%)] sm:h-64 md:h-28 lg:block" />
       </div>
-      <div className="w-3/4 bg-landing-brown-300 p-16 text-landing-brown-400 lg:bg-landing-brown-300 lg:text-white" />
+      <div className="w-4/5 bg-landing-brown-300 p-16 text-landing-brown-400 lg:bg-landing-brown-300 lg:text-white" />
     </div>
   );
 };
