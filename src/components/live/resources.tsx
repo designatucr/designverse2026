f2d048fd066e3859f808003f1755402c0e4eb290
Kevin Loritsch <kloritsch58@gmail.com>
2026-03-14 15:22:59 -0700

    fixes pt 1

--- src/components/live/resources.tsx
+++ src/components/live/resources.tsx
@@ -51,9 +51,15 @@ const bakeryItems = [
   },
 ];
 
+const shelfClipPath = [
+  "[clip-path:polygon(12%_0%,100%_0%,100%_100%,0%_100%)]",
+  "[clip-path:polygon(-20%_0%,120%_0%,120%_100%,-20%_100%)]",
+  "[clip-path:polygon(0%_0%,88%_0%,100%_100%,0%_100%)]",
+];
+
 const Resources = () => {
   return (
-    <div className="flex flex-col items-center justify-center p-16">
+    <div className="flex flex-col items-center justify-center p-6 font-poppins md:p-16">
       <p className="flex w-4/5 justify-center rounded-t-2xl bg-landing-brown-300 p-12 font-inter text-2xl font-bold text-white md:text-7xl">
         RESOURCES
       </p>
@@ -64,30 +70,35 @@ const Resources = () => {
               className="relative flex flex-col items-center justify-center"
               key={index}
             >
-              <div className="relative flex h-[20vh] w-full flex-col items-center justify-center bg-landing-glass-100/15 sm:h-[25vh] md:h-[40vh] lg:h-[40vh]">
+              <div className="relative flex h-[20vh] w-full flex-col items-center justify-center bg-landing-glass-100/15 md:h-[25vh] lg:h-[30vh]">
                 <Link
                   href={link}
                   target="_blank"
-                  className="flex w-full flex-col items-center justify-center hover:scale-105 hover:cursor-pointer"
+                  className="flex w-full flex-col items-center justify-center"
                 >
                   <Image
                     src={image}
-                    className="md:w-42 w-24 rounded-full sm:w-32 lg:w-32"
+                    className="md:w-42 sm:w-30 w-24 rounded-full transition hover:scale-105 hover:cursor-pointer lg:w-32"
                     alt="Resource"
                   />
                 </Link>
+                <div
+                  className={`sm:h-21 absolute -left-px bottom-0 -z-10 h-[32%] w-[calc(100%+2px)] translate-y-[1%] bg-landing-brown-400 md:h-[32%] lg:block ${shelfClipPath[index]} hidden`}
+                />
                 <div className="sm:h-21 absolute bottom-0 -z-10 h-[32%] w-full translate-y-[1%] bg-landing-brown-400 [clip-path:polygon(12%_0%,87%_0%,_100%_100%,_0%_100%)] md:h-[32%] lg:hidden" />
               </div>
-              <div className="h-2/3 w-full bg-landing-brown-300 p-8 text-center text-white lg:p-10">
-                <div className="mb-6 text-xl font-bold md:text-3xl">
+              <div className="flex h-2/3 w-full flex-col items-center justify-center bg-landing-brown-300 px-8 text-center text-white lg:px-10">
+                <div className="mb-6 font-inter text-xl font-bold md:text-3xl">
                   {title}
                 </div>
-                <div className="text-center lg:text-lg">{description}</div>
+                <div className="text-center font-inter lg:text-lg">
+                  {description}
+                </div>
               </div>
             </div>
           ))}
           <div className="w-full bg-landing-brown-300 md:hidden lg:bg-gray-100" />
-          <div className="relative flex h-[20vh] w-full flex-col items-center justify-center bg-landing-glass-100/15 sm:h-[25vh] md:h-[40vh] lg:hidden lg:h-[40vh]">
+          <div className="relative flex h-[20vh] w-full flex-col items-center justify-center bg-landing-glass-100/15 sm:h-[20vh] md:h-[40vh] lg:hidden">
             <Image
               src={Turtle}
               alt="Turtle Bread"
@@ -104,7 +115,6 @@ const Resources = () => {
             </div>
           ))}
         </div>
-        <div className="absolute top-[21%] -z-10 hidden h-[32%] w-full translate-y-[1%] bg-landing-brown-400 [clip-path:polygon(6%_0%,94%_0%,_100%_100%,_0%_100%)] sm:h-64 md:h-28 lg:block" />
         <div className="absolute bottom-0 -z-10 hidden h-[32%] w-full translate-y-[3%] bg-landing-brown-400 [clip-path:polygon(6%_0%,94%_0%,_100%_100%,_0%_100%)] sm:h-64 md:h-28 lg:block" />
       </div>
       <div className="w-4/5 bg-landing-brown-300 p-16 text-landing-brown-400 lg:bg-landing-brown-300 lg:text-white" />
--- src/data/sponsors/sponsors.ts
+++ src/data/sponsors/sponsors.ts
@@ -1,81 +1,82 @@
 import { StaticImageData } from "next/image";
-import Sponsor1 from "@/public/landing/sponsor1.svg";
-import Sponsor2 from "@/public/landing/sponsor2.svg";
-// import Sponsor3 from "@/public/landing/sponsor3.svg";
-// import Sponsor4 from "@/public/landing/sponsor4.svg";
-import Sponsor5 from "@/public/landing/sponsor5.svg";
-import Sponsor6 from "@/public/landing/sponsor6.svg";
-import Sponsor7 from "@/public/landing/sponsor7.svg";
-import Sponsor8 from "@/public/landing/sponsor8.svg";
-import Sponsor9 from "@/public/landing/sponsor9.svg";
-import Sponsor10 from "@/public/landing/sponsor10.svg";
+import Snapchat from "@/public/landing/sponsors/Snapchat.svg";
+import CreatRLab from "@/public/landing/sponsors/CreatRLab.svg";
+// import PG from "@/public/landing/sponsors/PG.svg";
+import ASUCR from "@/public/landing/sponsors/ASUCR.svg";
+import SoB from "@/public/landing/sponsors/SoB.svg";
+import Notion from "@/public/landing/sponsors/Notion.svg";
+import Framer from "@/public/landing/sponsors/Framer.svg";
+import Axure from "@/public/landing/sponsors/Axure.svg";
+import Balsamiq from "@/public/landing/sponsors/Balsamiq.svg";
+import Pure from "@/public/landing/sponsors/Pure.svg";
+import Monster from "@/public/landing/sponsors/Monster.svg";
+import Canes from "@/public/landing/sponsors/Canes.svg";
 
 export interface Sponsor {
   image: StaticImageData;
   name: string;
-  translateY: boolean;
   link: string;
 }
 
 export const sponsors: Sponsor[] = [
   {
-    image: Sponsor1,
+    image: Snapchat,
     name: "Snapchat",
-    translateY: false,
     link: "https://www.snapchat.com/",
   },
   {
-    image: Sponsor2,
+    image: CreatRLab,
     name: "Creat'R Lab",
-    translateY: true,
     link: "https://library.ucr.edu/research-support/making-and-innovation/creatr-lab-makerspace",
   },
   // {
-  //   image: Sponsor3,
+  //   image: PG,
   //   name: "Progressive Graphics",
-  //   translateY: false,
   //   link: "https://progressive-graphics.com/Progressive_Graphics/shop/home",
   // },
-  // {
-  //   image: Sponsor4,
-  //   name: "Associated Students of UCR",
-  //   translateY: false,
-  //   link: "https://asucr.ucr.edu/",
-  // },
   {
-    image: Sponsor5,
+    image: ASUCR,
+    name: "Associated Students of UCR",
+    link: "https://asucr.ucr.edu/",
+  },
+  {
+    image: SoB,
+    name: "UCR School of Business",
+    link: "https://business.ucr.edu/",
+  },
+  {
+    image: Notion,
     name: "Notion",
-    translateY: false,
     link: "https://www.notion.com/",
   },
   {
-    image: Sponsor6,
-    name: "Monster Energy",
-    translateY: true,
-    link: "https://www.monsterenergy.com/en-us/",
-  },
-  {
-    image: Sponsor8,
+    image: Framer,
     name: "Framer",
-    translateY: false,
     link: "https://www.framer.com/",
   },
   {
-    image: Sponsor9,
-    name: "Azure RP",
-    translateY: false,
+    image: Axure,
+    name: "Axure RP",
     link: "https://www.axure.com/",
   },
   {
-    image: Sponsor7,
+    image: Balsamiq,
     name: "Balsamiq",
-    translateY: false,
     link: "https://balsamiq.com/",
   },
   {
-    image: Sponsor10,
+    image: Pure,
     name: "Pure Buttons",
-    translateY: false,
     link: "https://www.purebuttons.com/",
+  },
+  {
+    image: Monster,
+    name: "Monster Energy",
+    link: "https://www.monsterenergy.com/en-us/",
+  },
+  {
+    image: Canes,
+    name: "Raising Cane's",
+    link: "https://raisingcanes.com/home/",
   },
 ];
